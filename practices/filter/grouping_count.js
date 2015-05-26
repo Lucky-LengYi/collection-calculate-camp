1112e8193a8700d898c80bca8f990ef25cfeec6e
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 11:17:13 +0800

    complete codes

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,24 +1,9 @@
 'use strict';
 
 function grouping_count(collection) {
-
     var _ = require('../lodash');
-    var uniq = _.uniq(collection);
-    var result = {};
-
-    for (var i = 0; i < uniq.length; i++) {
-        result.uniq[i] = 0;
-    }
-
-
-
-    // for (var i = 0; i < collection.length; i++) {
-    //     for (var x = 0; x < uniq.length; x++) {
-    //         if (collection[i] === uniq[x]) {
-    //             result.push({uniq[x]});
-    //         }
-    //     }
-    // }
+    
+    return _.countBy(collection);
 }
 
 module.exports = grouping_count;
--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+    var _ = require('../lodash');
+
+    var sum = 0;
+    for (var i = 0; i < collection.length; i++) {
+        sum = sum + collection[i];
+    }
+    var average = sum/collection.length;
+    return average;
 }
 
 module.exports = compute_average;
-
