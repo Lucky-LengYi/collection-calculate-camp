ebbaa37e99f97298fdd03326e4b5000f3d35e68e
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 16:34:26 +0800

    add code

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,9 +2,7 @@
 
 function compute_average(collection) {
     var _ = require('../lodash');
-
-
-
+    
     var sum = _.sum(collection);
     var average = sum/collection.length;
     return average;
