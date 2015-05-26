a065662c553b409a714a4a7ba84063b8aa82380a
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 10:03:45 +0800

    mark,not complete

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,23 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
+    var _ = require('../lodash');
+    var uniq = _.uniq(collection);
+    var result = {};
+
+    for (var i = 0; i < uniq.length; i++) {
+        result.uniq[i] = 0;
+    }
+
+
+
+    // for (var i = 0; i < collection.length; i++) {
+    //     for (var x = 0; x < uniq.length; x++) {
+    //         if (collection[i] === uniq[x]) {
+    //             result.push({uniq[x]});
+    //         }
+    //     }
+    // }
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -2,7 +2,6 @@
 
 function choose_divisible_integer(collection_a, collection_b) {
 
-  //在这里写入代码
 }
 
 module.exports = choose_divisible_integer;
