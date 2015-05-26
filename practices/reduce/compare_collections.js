003fce6474f811921b5d4adbdf5fcc752d343941
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 10:44:59 +0800

    git complete reduce 2,3

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,9 +1,14 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+    var object_a = collection_a.toString();
+    var object_b = collection_b.toString();
+
+    if (object_a === object_b) {
+        return true;
+    }else {
+        return false;
+    }
 }
 
 module.exports = compare_collections;
-
-
