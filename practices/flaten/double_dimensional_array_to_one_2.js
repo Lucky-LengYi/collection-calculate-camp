efb710705c5d8dfc0e5a07171eaf09883d09b114
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 10:11:41 +0800

    complete flaten

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function double_to_one(collection) {
+    var _ = require('../lodash');
 
-  //在这里写入代码
+    var one_dimensional_array = _.flattenDeep(collection);
+
+    return _.uniq(one_dimensional_array);
 }
 
 module.exports = double_to_one;
