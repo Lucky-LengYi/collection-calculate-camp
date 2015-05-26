656c7cf26ad1572b1b3c3de78622863ce91f445e
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 10:02:43 +0800

    update

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-  var _ = require('../lodash');
-  return _.difference(collection_a, collection_b);
+    var _ = require('../lodash');
+    return _.difference(collection_a, collection_b);
 }
 
 module.exports = choose_no_common_elements;
