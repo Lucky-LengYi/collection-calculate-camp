eb4c1008b76b2276571eb2f4ef751d3cdba37a91
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-26 11:36:42 +0800

    complete codes

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -2,6 +2,17 @@
 
 function choose_divisible_integer(collection_a, collection_b) {
 
+    var result = [];
+
+    for (var i = 0; i < collection_a.length; i++) {
+        for (var x = 0; x < collection_b.length; x++) {
+            if (collection_a[i]%collection_b[x] === 0) {
+                result.push(collection_a[i]);
+            }
+        }
+    }
+
+    return result;
 }
 
 module.exports = choose_divisible_integer;
