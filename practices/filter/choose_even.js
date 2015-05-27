344a6fb2fa6f6cb44d623ba86755eef20c8211cc
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-27 11:09:58 +0800

    complete choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,7 +1,19 @@
 'use strict';
 
+function get_even(array,num) {
+    if (num%2 === 0) {
+        array.push(num);
+    }
+    return array;
+}
+
 function choose_even(collection) {
-    
+    var array = [];
+
+    for (var i = 0; i < collection.length; i++) {
+        array = get_even(array,collection[i]);
+    }
+    return array;
 }
 
 module.exports = choose_even;
