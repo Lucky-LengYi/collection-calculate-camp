fdeed76e9f2b66ed48664b675100384412219d5d
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-27 10:10:22 +0800

    update codes

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,9 +1,22 @@
 'use strict';
+function get_same_number(array,num_a,num_b) {
+    if (num_a === num_b) {
+        array.push(num_a);
+    }
+    return array;
+}
 
 function get_intersection(collection_a, collection_b) {
-    var _ = require('../lodash');
 
-    return _.intersection(collection_b, collection_a);
+    var result = [];
+
+    for (var i = 0; i < collection_b.length; i++) {
+        for (var x = 0; x < collection_a.length; x++) {
+            result = get_same_number(result,collection_b[i],collection_a[x]);
+        }
+    }
+
+    return result;
 }
 
 module.exports = get_intersection;
