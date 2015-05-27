a3f14b8c40a0c98d64d5a3d81379411e63d6c898
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-27 11:12:21 +0800

    complete choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,7 +1,19 @@
 'use strict';
 
+function get_three_times(array,num) {
+    if (num%3 === 0) {
+        array.push(num);
+    }
+    return array;
+}
+
 function choose_multiples_of_three(collection) {
-    
+    var array = [];
+
+    for (var i = 0; i < collection.length; i++) {
+        array = get_three_times(array,collection[i]);
+    }
+    return array;
 }
 
 module.exports = choose_multiples_of_three;
