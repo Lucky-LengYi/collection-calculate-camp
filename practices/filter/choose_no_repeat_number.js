8c3343249a8584fbf4bfe985d80dff236cbd7ba8
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-27 11:47:19 +0800

    complete choose_no_repeat_number

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,7 +1,25 @@
 'use strict';
+function no_repeat_number(array,num) {
+
+    var exist = false;
+    for (var i = 0; i < array.length; i++) {
+        exist = num === array[i]?true:false;
+    }
+    if (!exist) {
+        array.push(num);
+    }
+
+    return array;
+}
 
 function choose_no_repeat_number(collection) {
-    
+    var array = [];
+
+    for (var i = 0; i < collection.length; i++) {
+        array = no_repeat_number(array,collection[i]);
+    }
+
+    return array;
 }
 
 module.exports = choose_no_repeat_number;
