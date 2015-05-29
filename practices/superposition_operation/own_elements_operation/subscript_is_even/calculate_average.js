74ed71586186a3e594b75ad8a20c7fb1be692206
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-29 13:13:12 +0800

    update the codes

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,10 @@
 'use strict';
 var calculate_average = function(collection){
-
+    var sum = 0;
+    for (var i = 0; i < collection.length; i++) {
+       sum += collection[i];
+    }
+    var average = sum / collection.length;
+    return average;
 };
 module.exports = calculate_average;
