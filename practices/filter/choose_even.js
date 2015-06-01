9ec73da0aa1da8fb69eb4ed827353e679a9411fd
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-01 17:06:34 +0800

    using my_lodash

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,19 +1,28 @@
 'use strict';
-
-function get_even(array,num) {
-    if (num%2 === 0) {
-        array[array.length] = num;
-    }
-    return array;
-}
+var _ = require('../my_lodash/my_lodash.js');
+// 
+// function get_even(array,num) {
+//     if (num%2 === 0) {
+//         array[array.length] = num;
+//     }
+//     return array;
+// }
 
 function choose_even(collection) {
-    var array = [];
+    // var array = [];
 
-    for (var i = 0; i < collection.length; i++) {
-        array = get_even(array,collection[i]);
-    }
-    return array;
+    return _.filter_in(collection,function (num) {
+        if (num % 2 === 0) {
+            return true;
+        }
+        return false;
+    });
+
+
+    // for (var i = 0; i < collection.length; i++) {
+    //     array = get_even(array,collection[i]);
+    // }
+    // return array;
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,19 +1,27 @@
 'use strict';
 
-function get_three_times(array,num) {
-    if (num%3 === 0) {
-        array[array.length] = num;
-    }
-    return array;
-}
+var _ = require('../my_lodash/my_lodash.js');
+// function get_three_times(array,num) {
+//     if (num%3 === 0) {
+//         array[array.length] = num;
+//     }
+//     return array;
+// }
 
 function choose_multiples_of_three(collection) {
-    var array = [];
+    // var array = [];
 
-    for (var i = 0; i < collection.length; i++) {
-        array = get_three_times(array,collection[i]);
-    }
-    return array;
+    return _.filter_in(collection,function (num) {
+        if (num % 3 === 0) {
+            return true;
+        }
+        return false;
+    });
+
+    // for (var i = 0; i < collection.length; i++) {
+    //     array = get_three_times(array,collection[i]);
+    // }
+    // return array;
 }
 
 module.exports = choose_multiples_of_three;
