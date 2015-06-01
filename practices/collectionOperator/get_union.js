7d9505e225ee07c1bc6b83816449c7d9cfa52ad0
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-01 17:02:53 +0800

    using my_lodash

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,25 +1,33 @@
 'use strict';
 
-function get_diff_num(object,num) {
-    var exist = false;
-    for (var i = 0; i < object.length; i++) {
-        if (object[i] === num) {
-            exist = true;
-            break;
-        }
-    }
-    if (!exist) {
-        object[object.length] = num;
-    }
-    return object;
-}
+var _ = require('../my_lodash/my_lodash.js');
+
+// function get_diff_num(object,num) {
+//     var exist = false;
+//     for (var i = 0; i < object.length; i++) {
+//         if (object[i] === num) {
+//             exist = true;
+//             break;
+//         }
+//     }
+//     if (!exist) {
+//         object[object.length] = num;
+//     }
+//     return object;
+// }
 
 function get_union(collection_a, collection_b) {
-    var result = [];
-    for (var i = 0; i < collection_b.length; i++) {
-        result = get_diff_num(collection_a,collection_b[i]);
-    }
+    var result = collection_a;
+    _.each(collection_b,function (item,i) {
+        if (!_.exist(result,item)) {
+            result[result.length] = item;
+        }
+    });
 
+    // for (var i = 0; i < collection_b.length; i++) {
+    //     result = get_diff_num(collection_a,collection_b[i]);
+    // }
+    //
     return result;
 }
 
