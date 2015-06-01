d85e75be57a529414b3fed5d38ecc837523b50d1
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-01 16:58:20 +0800

    uodate my_lodash

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,20 +1,13 @@
 'use strict';
-function get_same_number(array,num_a,num_b) {
-    if (num_a === num_b) {
-        array[array.length] = num_a;
-    }
-    return array;
-}
+var _ = require('../my_lodash/my_lodash.js');
 
 function get_intersection(collection_a, collection_b) {
-
     var result = [];
-
-    for (var i = 0; i < collection_b.length; i++) {
-        for (var x = 0; x < collection_a.length; x++) {
-            result = get_same_number(result,collection_b[i],collection_a[x]);
+    _.each(collection_b,function (item,i) {
+        if (_.exist(collection_a,item)) {
+            result[result.length] = item;
         }
-    }
+    });
 
     return result;
 }
