7f654cc1f6f28cc8e7ee2290d0dfa3ed732dca9b
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-02 20:05:04 +0800

    extrect code

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,13 +1,6 @@
 'use strict';
 var _ = require('../my_lodash/my_lodash.js');
 
-// function get_same_letter(array,letter_a,letter_b) {
-//     if (letter_a === letter_b) {
-//         array[array.length] = letter_a;
-//     }
-//     return array;
-// }
-
 function choose_common_elements(collection_a, collection_b) {
     var result = [];
     _.each(collection_a,function (letter) {
@@ -16,11 +9,6 @@ function choose_common_elements(collection_a, collection_b) {
         }
     });
 
-    // for (var i = 0; i < collection_a.length; i++) {
-    //     for (var x = 0; x < collection_b.length; x++) {
-    //         result = get_same_letter(result,collection_a[i],collection_b[x]);
-    //     }
-    // }
     return result;
 }
 
--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,4 +1,5 @@
 'use strict';
+var _ = require('../my_lodash/my_lodash.js');
 function get_diff_letter(array,letter,collection) {
     var judge = true;
     for (var i = 0; i < collection.length; i++) {
@@ -11,11 +12,20 @@ function get_diff_letter(array,letter,collection) {
     }
     return array;
 }
+
 function choose_no_common_elements(collection_a, collection_b) {
     var result = [];
-    for (var i = 0; i < collection_a.length; i++) {
-        result = get_diff_letter(result,collection_a[i],collection_b);
-    }
+
+    _.each(collection_a,function (letter) {
+        if (!_.exist(collection_b,letter)) {
+            result[result.length] = letter;
+        }
+    });
+
+
+    // for (var i = 0; i < collection_a.length; i++) {
+    //     result = get_diff_letter(result,collection_a[i],collection_b);
+    // }
     return result;
 }
 
