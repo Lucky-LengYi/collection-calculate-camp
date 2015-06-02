e2a93718adc07c821a8cc4dd9d4d0b953d46ce18
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-02 22:15:48 +0800

    change code to using my_lodash

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,16 +1,16 @@
 'use strict';
-
+var _ = require('../../my_lodash/my_lodash.js');
 function average_uneven(collection) {
-    var sum = 0;
-    var item = 0;
-    for (var i = 0; i < collection.length; i++) {
-        if (collection[i] % 2 === 1) {
-            sum += collection[i];
-            item ++;
+    var result_array = _.filter_in(collection,function (num) {
+        if (num % 2 === 1) {
+            return true;
+        }else {
+            return false;
         }
-    }
-    var average = sum / item;
-    return average;
+    });
+
+    var sum = _.sum(result_array);
+    return sum / result_array.length;
 }
 
 module.exports = average_uneven;
