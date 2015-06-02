3c7461c441cf2076624ccd37579021c52a19b34f
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-02 22:08:37 +0800

    change code to using my_lodash

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,11 +1,17 @@
 'use strict';
+var _ = require('../../my_lodash/my_lodash.js');
+
 function hybrid_operation_to_uneven(collection) {
-    var result = [];
-    for (var i = 0; i < collection.length; i++) {
-        if (collection[i]%2 === 1) {
-            result[result.length] = collection[i] * 3 + 2;
+    var get_odd = _.filter_in(collection,function (num) {
+        if (num % 2 === 1) {
+            return true;
+        }else {
+            return false;
         }
-    }
+    });
+    var result = _.map(get_odd,function (num) {
+        return num * 3 + 2;
+    });
     return result;
 }
 
--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,13 +1,18 @@
 'use strict';
+var _ = require('../../my_lodash/my_lodash.js');
 
 function hybrid_operation_to_uneven(collection) {
-    var sum = 0;
-    for (var i = 0; i < collection.length; i++) {
-        if (collection[i]%2 === 1) {
-            sum += collection[i] * 3 + 5;
+    var get_odd = _.filter_in(collection,function (num) {
+        if (num % 2 === 1) {
+            return true;
+        }else {
+            return false;
         }
-    }
-    return sum;
+    });
+    var result = _.map(get_odd,function (num) {
+        return num * 3 + 5;
+    });
+    return _.sum(result);
 }
 
 module.exports = hybrid_operation_to_uneven;
