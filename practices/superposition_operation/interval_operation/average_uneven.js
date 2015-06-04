6b46ad13a069ebfb388176e1199244bf8c99732b
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-04 16:34:35 +0800

    check the code && clean code

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -2,11 +2,7 @@
 var _ = require('../../my_lodash/my_lodash.js');
 function average_uneven(collection) {
     var result_array = _.filter_in(collection,function (num) {
-        if (num % 2 === 1) {
-            return true;
-        }else {
-            return false;
-        }
+        return num % 2 === 1;
     });
     var sum = _.sum(result_array);
     return sum / result_array.length;
--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,39 +1,12 @@
 'use strict';
-function bubble_sort(object) {
-    var num;
-    for (var i = 0; i < object.length; i++) {
-        for (var x = i + 1; x < object.length; x++) {
-            if (object[x] < object[i]) {
-                num = object[i];
-                object[i] = object[x];
-                object[x] = num;
-            }
-        }
-    }
-    return object;
-}
-function median_to_letter(collection) {
-    var median;
-    var NUMBER_OF_LETTERS = 26;
-    collection = bubble_sort(collection);
-    if (collection.length%2 === 0) {
-        median = (collection[collection.length/2] + collection[collection.length/2-1])/2;
-    }else {
-        median = collection[collection.length/2-0.5];
-    }
-    median = Math.ceil(median);
+var _ = require('../../my_lodash/my_lodash.js');
 
-    var letter = '';
-    if (median/NUMBER_OF_LETTERS > 1) {
-        var first_letter = String.fromCharCode(Math.ceil(median/NUMBER_OF_LETTERS)+95);
-        var num = median % NUMBER_OF_LETTERS;
-        num = num === 0? NUMBER_OF_LETTERS:num;
-        var second_letter = String.fromCharCode(num + 96);
-        letter = first_letter + second_letter;
-    }else {
-        letter = String.fromCharCode(median+96);
-    }
-    return letter;
+function median_to_letter(collection) {
+    var result = _.bubble_sort(collection);
+    var median = _.get_median(result);
+    median = Math.ceil(median);
+    var median_letter = _.num_to_letter(median);
+    return median_letter;
 }
 
 module.exports = median_to_letter;
