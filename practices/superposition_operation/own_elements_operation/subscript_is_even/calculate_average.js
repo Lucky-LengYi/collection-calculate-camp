22857c70e3ef7be8f4c27fc93c58f5ca0131c27f
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-04 16:45:14 +0800

    check the code && clean code

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,14 +1,11 @@
 'use strict';
+var _ = require('../../../my_lodash/my_lodash.js');
+
 var calculate_average = function(collection){
-    var sum = 0;
-    var item = 0;
-    for (var i = 0; i < collection.length; i++) {
-        if (i % 2 === 1) {
-            sum += collection[i];
-            item += 1;
-        }
-    }
-    var average = sum / item;
-    return average;
+    var result = _.filter(collection,function (num , i) {
+        return i % 2 === 1;
+    });
+    var sum = _.sum(result);
+    return sum / result.length;
 };
 module.exports = calculate_average;
